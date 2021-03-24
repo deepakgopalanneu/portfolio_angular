e85b64e19a37c8baf7b5518df914dafe8ed0a12f
Deepak Gopalan <gopalan.d@northeastern.edu>
2021-03-24 17:24:27 -0400

    only send email-lambda call pending

--- src/app/contactme/contactme.component.ts
+++ src/app/contactme/contactme.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl,FormBuilder, Validators, FormGroup } from '@angular/forms';
 
@@ -11,10 +12,10 @@ export class ContactmeComponent implements OnInit {
   myEmailId = "gopalan.d@northeastern.edu"
   form :FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private http :HttpClient) {
     this.form = this.formBuilder.group({
-      messageControl: new FormControl('', 
-            Validators.compose([Validators.required, Validators.minLength(20), Validators.maxLength(1000)]))
+      messageControl: new FormControl('', Validators.compose([Validators.required, Validators.minLength(20), Validators.maxLength(1000)])),
+      emailControl: new FormControl('', Validators.compose([Validators.required, Validators.email])),
     });
   }
 
@@ -28,5 +29,6 @@ export class ContactmeComponent implements OnInit {
 
   sendEmail(){
     // call API
+    // this.http.post("")
   }
 }
